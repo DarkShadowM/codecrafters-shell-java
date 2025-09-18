2b684c05e22f9edcb0e08cae472777a9f106e5c5
DarkShadowM <krishna.maurya0123@gmail.com>
2025-09-18 15:14:23 +0530

    Run a program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -20,14 +23,15 @@ public class Main {
             String command = commands[0];
             String argument = commands.length > 1 ? commands[1] : "";
 
-            if(command.equals(PreDefinedCommands.ECHO.getValue())){
+            if(input.startsWith(PreDefinedCommands.ECHO.getValue())){
                 handleEcho(argument);
             }
-            else if(command.equals(PreDefinedCommands.TYPE.getValue())){
+            else if(input.startsWith(PreDefinedCommands.TYPE.getValue())){
                 handleType(argument);
             }
             else {
-                System.out.println(input + ": command not found");
+                handleRunProgram(command, argument);
+
             }
         }
 
@@ -35,9 +39,24 @@ public class Main {
         scanner.close();
     }
 
+    private static void handleRunProgram(String command, String argument) {
+        List<String> commands = new ArrayList<>();
+        try {
+        commands.add(command);
+        commands.addAll(Arrays.asList(argument.split(" ")));
+        ProcessBuilder pb = new ProcessBuilder(commands);
+            pb.inheritIO(); // inherit stdin, stdout, stderr
+            Process proc = pb.start();
+            proc.waitFor();
+        } catch (IOException e) {
+            System.out.println(command + ": command not found");
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
 
 
-
+    }
 
 
     private static void handleType(String argument) {
